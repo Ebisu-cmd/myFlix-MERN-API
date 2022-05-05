d280579bccc17fdddf2d9b32b43f74b97700c024
Gabriel Jardio <gabejardio@gmail.com>
2022-05-04 22:31:02 -0500

    When updating user information, password is also hashed

--- auth.js
+++ auth.js
@@ -20,10 +20,7 @@ module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', {session: false}, (error, user, info) => {
             if (error || !user) {
-                return res.status(400).json({
-                    message: 'Something is not right',
-                    user: user
-                });
+                return res.status(400).json({ info });
             }
             req.login(user, {session: false}, (error) => {
                 if (error) {
